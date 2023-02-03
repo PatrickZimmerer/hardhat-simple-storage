f958414d0a1e25bbd5d44504b9e46f28668a5a5c
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-03 23:42:08 +0100

    added localhost to networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
+import "./tasks/block-number.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -16,6 +17,11 @@ const config: HardhatUserConfig = {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY!],
             chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts get handled by hardhat on localhost (through yarn hardhat node)
+            chainId: 31337,
         },
     },
     solidity: "0.8.8",
