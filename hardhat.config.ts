cb0d8d00df6d77dddd193335de708b0ce85cbcfe
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-04 00:48:08 +0100

    added solidity-coverage and removed unused imports

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-etherscan";
 import "hardhat-gas-reporter";
+import "solidity-coverage";
 import "./tasks/block-number.js";
 import * as dotenv from "dotenv";
 dotenv.config();
