866cd210d52a26300b15732041dbe356d9bd9c17
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-03 23:42:45 +0100

    added verify function when deploying to goerli testnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,6 @@
-import { ethers } from "hardhat";
+import { ethers, run, network } from "hardhat";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,6 +10,33 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy();
     await simpleStorage.deployed();
     console.log("deployed to", simpleStorage.address);
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6);
+        await verify(simpleStorage.address, []);
+    }
+
+    const currentValue = await simpleStorage.retrieve();
+    console.log("current value is:", currentValue);
+    const transactionResponse = await simpleStorage.store(7);
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.retrieve();
+    console.log("updated value is:", updatedValue);
+}
+
+async function verify(contractAddress: any, args: any) {
+    console.log("Veryfying contract...", { contractAddress, args });
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        });
+    } catch (error: any) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!");
+        } else {
+            console.log(error);
+        }
+    }
 }
 
 main()
