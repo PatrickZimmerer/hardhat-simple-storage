51c8d7beb41669816a8b51a0482e95471a8072a3
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-02-03 22:26:25 +0100

    copied in old contract and basic setup to deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,18 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    );
+    console.log("deplyoing contract... wait please...");
+    const simpleStorage = await SimpleStorageFactory.deploy();
+    await simpleStorage.deployed();
+    console.log("deployed to", simpleStorage.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error);
+        process.exit(1);
+    });
